c123e0f1bf783d572c501839cd58148f36a10a55
Christopher Locke <clocke@degica.com>
2014-08-20 00:08:05 +0900

    Refactor FakeFullslate stub router

--- spec/support/fake_fullslate.rb
+++ spec/support/fake_fullslate.rb
@@ -2,75 +2,34 @@ require 'sinatra/base'
 
 class FakeFullslate < Sinatra::Base
 
-  #
-  # Fullslate::Employee API calls
-  #
-  get '/api/employees' do
-    json_response 200, 'employees.json'
-  end
+  # Generate the routing for each type of API call
+  ['employees', 'services', 'clients', 'events'].each do |str|
 
-  get '/api/employees/:id' do
-    json = json_response 200, 'employees.json'
-
-    employee = JSON.parse(json).find do |employee_json|
-      employee_json["id"] == params[:id].to_i
+    get "/api/#{str}" do
+      objects_call(str, params)
     end
 
-    employee.to_json
-  end
-
-  #
-  # Fullslate::Service API calls
-  #
-  get '/api/services' do
-    json_response 200, 'services.json'
-  end
-
-  get '/api/services/:id' do
-    json = json_response 200, 'services.json'
-
-    service = JSON.parse(json).find do |services_json|
-      services_json["id"] == params[:id].to_i
+    get "/api/#{str}/:id" do
+      object_call(str, params)
     end
 
-    service.to_json
-  end
-
-  #
-  # Fullslate::Client API calls
-  #
-  get '/api/clients' do
-    json_response 200, 'clients.json'
-  end
-
-  get '/api/clients/:id' do
-    json = json_response 200, 'clients.json'
-
-    client = JSON.parse(json).find do |clients_json|
-      clients_json["id"] == params[:id].to_i
-    end
-
-    client.to_json
-  end
-
-  #
-  # Fullslate::Event API calls
-  #
-  get '/api/events' do
-    json_response 200, 'events.json'
-  end
-
-  get '/api/events/:id' do
-    json = json_response 200, 'events.json'
-
-    event = JSON.parse(json).find do |events_json|
-      events_json["id"] == params[:id].to_i
-    end
-
-    event.to_json
   end
 
   private
+
+  def objects_call(name, params)
+    json_response 200, "#{name}.json"
+  end
+
+  def object_call(name, params)
+    json = json_response 200, "#{name}.json"
+
+    obj = JSON.parse(json).find do |cur_json|
+      cur_json["id"] == params[:id].to_i
+    end
+
+    obj.to_json
+  end
 
   def json_response(response_code, file_name)
     content_type :json
